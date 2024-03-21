695410017379a9f546f64a70f71aa74dc1d9e525
Samandar <iwtuz166@gmail.com>
2024-03-21 15:54:43 +0500

    change urls

--- client/composables/baseUrl.js
+++ client/composables/baseUrl.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:3000/api";
-export const CDN_URL = "https://cdn.mel-book.uz/api";
-
-// export const BASE_URL = "https://api.mobile.mel-book.uz/api/";
+// export const BASE_URL = "http://localhost:3000/api";
 // export const CDN_URL = "https://cdn.mel-book.uz/api";
+
+export const BASE_URL = "https://api.mobile.mel-book.uz/api/";
+export const CDN_URL = "https://cdn.mel-book.uz/api";
